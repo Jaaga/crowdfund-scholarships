4434c046ace43b595b71981db1c1fea528f42498
manojrammurthy <manrox.drag@gmail.com>
2014-02-14 02:39:49 -0500

    checklogin and index

---
+++ checklogin.php
@@ -0,0 +1,39 @@
+<?php
+
+$host="localhost"; // Host name 
+$username="root"; // Mysql username 
+$password="123"; // Mysql password 
+$db_name="LearnEmp"; // Database name 
+$tbl_name="donor"; // Table name 
+
+// Connect to server and select databse.
+mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
+
+// username and password sent from form 
+$myusername=$_POST['email']; 
+$mypassword=$_POST['pass']; 
+
+// To protect MySQL injection (more detail about MySQL injection)
+$myusername = stripslashes($myusername);
+$mypassword = stripslashes($mypassword);
+$myusername = mysql_real_escape_string($myusername);
+$mypassword = mysql_real_escape_string($mypassword);
+$sql="SELECT * FROM $tbl_name WHERE email='$myusername' and password='$mypassword'";
+$result=mysql_query($sql);
+
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
+
+// If result matched $myusername and $mypassword, table row must be 1 row
+if($count==1){
+
+// Register $myusername, $mypassword and redirect to file "login_success.php"
+session_register("myusername");
+session_register("mypassword"); 
+header("location:Students_list.php");
+}
+else {
+echo "Wrong Username or Password";
+}
+?>
