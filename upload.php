5e4a9154017893cea0eacfaa096ae2ee47e15e84
Sai Prashanth <saiprashanth93@gmail.com>
2014-02-26 17:23:53 +0530

    third version

--- upload.php
+++ upload.php
@@ -1,10 +1,16 @@
-
-
+<html>
+<body>
+ <div class="col-md-4"><form action="upload.php" method="post" enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file">
+<input type="submit" name="submit" value="Submit"></form>
+</div>
+</body>
+</html>
  
 <?php
 
 
-echo $_FILES["file"]["error"];
 if ($_FILES["file"]["error"] > 0)
   {
   echo "Error: " . $_FILES["file"]["error"] . "<br>";
