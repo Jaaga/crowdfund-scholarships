7467750b01ed2b709782ae3dc0bc4edd4703f256
Sai Prashanth <saiprashanth93@gmail.com>
2014-02-28 01:34:23 +0530

    added student login

--- signin_stud.php
+++ signin_stud.php
@@ -1,56 +1,6 @@
 <?php
-$con=mysqli_connect("localhost","root","123","LearnEmp");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
 
-$sql="INSERT INTO donor (name, password,email)
-VALUES
-('$_POST[name]','$_POST[password]','$_POST[email]')";
-
-if (!mysqli_query($con,$sql))
-  {
-  die('Error: ' . mysqli_error($con));
-  }
-
-
-
-  ?>
-
-
-<html>
-
-<head>
-     <link href="./dist/css/bootstrap.css" rel="stylesheet">
-    
-
-    <!-- Custom styles for this template -->
-    <link href="jumbotron.css" rel="stylesheet">
-</head>
-
-<body>
-
-    
-       <div class="jumbotron" style="background color : blue" > 
-    <div class="container">
-    
-      <div class="row">
-       
-       <p align="center" style="font-family: verdana;">Signed-up Successfully</p>
-       
-       </div>
-
-    </div>
-    </div>
-          <hr>
-    <h6 align="center"> This Page will be redirected in few seconds...</h6>
-</body>
-</html>
-
-<?php
-       $host="localhost"; // Host name 
+$host="localhost"; // Host name 
 $username="root"; // Mysql username 
 $password="123"; // Mysql password 
 $db_name="LearnEmp"; // Database name 
@@ -59,29 +9,44 @@ $tbl_name="student"; // Table name
 // Connect to server and select databse.
 mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
 mysql_select_db("$db_name")or die("cannot select DB");
-   
-    
-          $sql1="SELECT * FROM donor WHERE email='$_POST[email]' and password='$_POST[password]'";
-          $result=mysql_query($sql1);
-          $row=mysql_fetch_array($result);
-          
-          $D_id=$row['S_id'];
-   
-          
-        
-  //   $count=mysql_num_rows($result);
+
+// username and password sent from form 
+$myusername=$_POST['email']; 
+$mypassword=$_POST['pass']; 
+
+// To protect MySQL injection (more detail about MySQL injection)
+$myusername = stripslashes($myusername);
+$mypassword = stripslashes($mypassword);
+$myusername = mysql_real_escape_string($myusername);
+$mypassword = mysql_real_escape_string($mypassword);
+$sql="SELECT * FROM $tbl_name WHERE email='$myusername' and password='$mypassword'";
+
+if ($sql!==1)
+{
+  echo "wrong username";
+}
+//$sql1="SELECT D_id FROM $tbl_name WHERE email='$myusername' ";
+$result=mysql_query($sql);
+$row=mysql_fetch_array($result);
+$S_id=$row['S_id'];
+//$result1=mysql_query($sql1);
+
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
 
 
 
 // If result matched $myusername and $mypassword, table row must be 1 row
-//if($count==1){
+if($count==1){
+session_start();
+$_SESSION['email']="$myusername";
 
-//// Register $myusername, $mypassword and redirect to file "login_success.php"
-//$_SESSION['usr']= "";
+// Register $myusername, $mypassword and redirect to file "login_success.php"
+//$_SESSION['usr']= "myusername";
 //$_SESSION['pawd']="mypassword"; 
- 
-header("Refresh: 3;url=Students_list.php?id=".$S_id);
- 
-     
-     mysqli_close($con);
+header("location:Students_list.php?id=$S_id&id2=$myusername");
+}
+else {
+echo "Wrong Username or Password";
+}
 ?>
