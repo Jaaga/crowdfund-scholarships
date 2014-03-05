6bb79e2ab22ee95998ef9f457578af4d63795240
manojrammurthy <manrox.drag@gmail.com>
2014-03-05 05:06:02 -0500

    checklogin fun

--- checklogin.php
+++ checklogin.php
@@ -1,14 +1,6 @@
 <?php
 
-$host="localhost"; // Host name 
-$username="root"; // Mysql username 
-$password="123"; // Mysql password 
-$db_name="LearnEmp"; // Database name 
-$tbl_name="donor"; // Table name 
-
-// Connect to server and select databse.
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
+include("dbcon.php");
 
 // username and password sent from form 
 $myusername=$_POST['email']; 
---
+++ src/dbcon.php
@@ -0,0 +1,12 @@
+<?php
+
+$host="localhost"; // Host name 
+$username="root"; // Mysql username 
+$password="123"; // Mysql password 
+$db_name="LearnEmp"; // Database name 
+$tbl_name="donor"; // Table name 
+
+// Connect to server and select databse.
+mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
+?>
