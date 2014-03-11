b1f15342252a65b7e79fbb45844e25733147bfba
abhinay302 <abhinay302@gmail.com>
2014-03-11 16:37:05 +0530

    Random

--- src/donor.php
+++ src/donor.php
@@ -13,21 +13,7 @@ if(!mysql_query($sql))
 	die('Error' .mysql_error());
 }
 
-<<<<<<< HEAD
-//if (!mysqli_query($con,$sql))
-  //{
-  //die('Error: ' . mysqli_error($con));
-//}
 }
-
-function donations($D_id) {
-	$con1 = mysql_connect("localhost","root","123") or die("Couldn't select DB");
-        $db = mysql_select_db("LearnEmp", $con1) or die("Couldn't select database."); 
-
-        $con2 = mysql_connect("localhost","root","123") or die("Couldn't select DB"); 
-        $db = mysql_select_db("LearnEmp", $con2)  or die("Couldn't select database."); 
-=======
->>>>>>> 06c858ac91aa79774c4ef3cb22fb46020b124395
 
 function getDonation(){
 
@@ -35,9 +21,8 @@ function getDonation(){
     $result=mysqli_query($sql);
 	$row=mysqli_fetch_array($result);
 
-	for ($row=0)
 }
 
 
-}
+
 ?>
