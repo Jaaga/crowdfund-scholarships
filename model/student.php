185d9843fd9d04b1c89662b63c578b7fe62cf79e
abhinay302 <abhinay302@gmail.com>
2014-03-18 21:39:48 +0530

    Student_list_Model

--- model/student.php
+++ model/student.php
@@ -2,65 +2,45 @@
   
   include ('dbcon.php');
 
-	function getStudentList()
-	{   
+    function getStudentList()
+    {   
         
         $db= dbopen();
-        $sql= "SELECT * from student";
-        $result= $db->query($sql);
-        $count=$result->num_rows;
-        
-        if (!$db->query($sql))
-        {
-        	die('Error'.$db->error());
+        $sql=$db->prepare('SELECT S_id, fname, lname FROM student');
+        $sql->execute();
+        $sql->bind_result($S_id,$fname,$lname);
+        while($sql->fetch()){
+               
+            $students[]=array('S_id'=>$S_id,'fname'=>$fname,'lname'=>$lname);
         }
+         //$sql->close();
+        return ($students);    
+    }
 
-        for($row=0;$row<$count;$row++)
-        {
-        	$data[$row][0]=$result['S_id'];
-        	$data[$row][1]=$result['fname'];
-        	$data[$row][2]=$result['lname'];
-        	$data[$row][3]=$result['gender'];
-        	$data[$row][4]=$result['email'];
-        	$data[$row][5]=$result['Phone_Number'];
-        	$data[$row][6]=$result['address'];
-        	$data[$row][7]=$result['pincode'];
-        	$data[$row][8]=$result['country'];
-        	$data[$row][9]=$result['course'];
-        	$data[$row][10]=$result['scholar_AMT'];
-        	$data[$row][11]=$result['para'];
-        	$data[$row][12]=$result['image_path'];
-        	
-        }
-
-         return ($data);   
-                
-	}
-
-	function studentLogin($email,$password)
-	{
-		$db= dbopen();
-		$sql="SELECT * from student where email='$email' && password='$password'";
+    function studentLogin($email,$password)
+    {
+        $db= dbopen();
+        $sql="SELECT * from student where email='$email' && password='$password'";
         $result= $db->query($sql);
 
-		$row=mysqli_fetch_array($result); 
+        $row=mysqli_fetch_array($result); 
 
-		$S_id = $row['S_id'];
+        $S_id = $row['S_id'];
 
-		$count = mysqli_num_rows($result);
+        $count = mysqli_num_rows($result);
         // If result matched $myusername and $mypassword, table row must be 1 row
-		if($count==1){
+        if($count==1){
 
-		    session_start();
+            session_start();
         // Register $myusername, $mypassword and redirect to file "Students_list.php"
-		    header("location:../public/Students_list.php?id=$D_id");
-		}
-		else{
-			
-			echo "wrong username or password";
+            header("location:../public/Students_list.php?id=$D_id");
+        }
+        else{
+            
+            echo "wrong username or password";
             header("Refresh: 2;url='../public/index.php'");
-		}
-	}
+        }
+    }
 
     function createStudent($studentId,$fname,$lname,$gender,$email,$Phone_Number,
         $address,$pincode,$country,$course,$scholar_AMT,$para,$password,$image_path){
--- model/test.php
+++ model/test.php
@@ -1,7 +1,25 @@
-<?php
-include('donor.php');
-//donorLogin($_POST['email'],$_POST['pass']);
-//giveDonation($_POST['3'],$_POST['8'],$_POST['20,000']);
-getStudents();
-
-?>
+<?php include ('student.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <!--other doc head stuff here-->
+</head>
+<body>
+    <nav><ol><li><a href="#">Home</a></li></ol></nav>
+    <!-- more body stuff blahh blahh-->
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>First name</th>
+            <th>Last name</th>
+        </tr>
+        <?php
+            $students = getStudentList();
+            foreach($students as $student){
+                echo"<tr><td>".$student['S_id']."</td><td>".$student['fname']."</td><td>".$student['lname']."</td></tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
