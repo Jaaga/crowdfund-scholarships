669b1cd288eeed38e628e8a35a92ab9090275b3a
Rajeef <rajeefmk@gmail.com>
2014-03-25 17:44:01 +0530

    Changed student dashboard layout to look similar to student profile

--- public/studentdashboard.php
+++ public/studentdashboard.php
@@ -2,226 +2,278 @@
 
 <head>
 	<title>Student Dashboard</title>
+
 	<link href="./dist/css/bootstrap.css" rel="stylesheet">
 	<link href="./dist/css/slider.css" rel="stylesheet">
-  <link href="./dist/css/custom.css" rel="stylesheet" type='text/css'>
-  
+	<link href="./dist/css/custom.css" rel="stylesheet" type='text/css'>
+	
 
 	<style>
+	
+	#bg{
+    background:url('./images/chalkboard2.jpg');
+    border: 3 solid /*#33cc33*/#6E8B3D;
+	color: #FFFFe0;
+	text-align: center;
+	}
 		body{
 background: url("./images/wood1.png");
 background-repeat:repeat;
 }
+.sponsor_data{
+font-family:'Cabin Sketch' cursive;
+font-size: 27px;
+}
 
-		#bg{
-    	background:url('./images/crowd_n.jpg');
-    	height: 500px;
-    	margin-top: 90px;
-		}
-
-	#student_dash{
-     background:url('./images/student.jpg');
-      
-      margin-top: 15px;
-      
-    }
-    
 	</style>
 </head>
 
 <body>
-
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-    
-      <div class="navbar-header" style="height: 50px;">
-    
-          
-            <a class="navbar-brand" href="home_v9.html"><h1 style="font-family:'Cabin Sketch' cursive; margin-top: -9px;">LearnEmp<h1></a>
-          </div>
-          <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="listofstudents_v8.html">Sponsor</a></li>
-                <li><a href="#">Sign Up</a></li>
-            </ul>
-
-        <div class="navbar-collapse collapse">
-              <form class="navbar-form navbar-right" role="form">
-                <div class="form-group">
-                  <input type="text" placeholder="Email" class="form-control">
-                </div>
-
-                <div class="form-group">
-                  <input type="password" placeholder="Password" class="form-control">
-                </div>
-
-                <button type="submit" class="btn btn-success" style="font-family: verdana;">Sign in</button> 
-                &nbsp 
-
-              </form>
-              
-              </div><!--/.navbar-collapse -->
-        </div>
-      </div>
-      </div> 
-	
-      
-    <br>
-    <br>
-    <br>
-
-	<div class="container">
-    	<div class="row">
-        	<div class="col-md-4">
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container">
+	  
+			<div class="navbar-header" style="height: 50px;">
 		
-				<div class="profilename">
-					<h3 align="left" style="font-family: Cabin Sketch;"> Welcome Vaibhav !</h3>
-				</div>
+          
+        		<a class="navbar-brand" href="home_v9.html"><h1 style="font-family:'Cabin Sketch' cursive; margin-top: -9px;">LearnEmp<h1></a>
+        	</div>
+        	<div class="collapse navbar-collapse">
+        		<ul class="nav navbar-nav">
+            		<li><a href="listofstudents_v8.html">Sponsor</a></li>
+            		<li><a href="#">Sign Up</a></li>
+        		</ul>
 
-				<br>
+				<div class="navbar-collapse collapse">
+        			<form class="navbar-form navbar-right" role="form">
+            		<div class="form-group">
+            			<input type="text" placeholder="Email" class="form-control">
+            		</div>
 
-				
-				<img src="./images/student.jpg"  style="height:200px; width:500px" id="student_dash" class="img-responsive">
-				
-				
+            		<div class="form-group">
+            			<input type="password" placeholder="Password" class="form-control">
+            		</div>
 
+            		<button type="submit" class="btn btn-success" style="font-family: verdana;">Sign in</button> 
+          			&nbsp 
+
+        			</form>
+           		
+           		</div><!--/.navbar-collapse -->
+    		</div>
+    	</div>
+    </div><!--/.navbar-collapse -->
+
+    <br>
+    <br>
+    <br>
+	<br>
+	<br>
+
+	<div class="container" >
+		<div class="row">
+			<div class="col-lg-3" align="left"  >
+			
+
+					<img src="./images/facebook-icon.png" width="25px"/>
+					<img src="./images/Twitter_logo.png" width="25px"/>
+					<img src="./images/linked-in.jpg" width="25px"/>
+					&nbsp
+
+					<a href="#">
+
+              		<span class="glyphicon glyphicon-edit" style="font-size:13px;"></span>
+        
+        			</a>
+					
+
+			</div>
+			<div class="col-lg-6">
+			<h1 align="center" style="font-family:'Cabin Sketch' cursive; font-size: 60px; margin-top: -30px " >
+			Vaibhav Mule
+			</h1>
+			</div>
+    		
+
+			<div class="col-lg-3" align="right" >
+   					
+       			<form action="application.php">
+
+       			<button type="submit" class="btn btn-success" style="font-family: verdana;" >Edit Application</button>
+    				
+    			</form>
+    		</div>	
+    	</div>
+
+    	<br>
+    
+
+    
+    	<div class="row">
+
+        	<div class="col-xs-7"  text-align="justify">
+        		
+
+				<img src="./images/student.jpg" width="100%" height="400px">
 				<input type="file" id="profile_pic" name="upload" style="visibility: hidden; width: 1px; height: 1px" multiple />
 
 				<a href="" onclick="document.getElementById('profile_pic').click(); return false">
 
-          		<span class="glyphicon glyphicon-edit"></span>
+          		<span class="glyphicon glyphicon-camera" style="font-size:20px;"></span>
 
         		</a>
-	 			 
+		 		<br>
+		 		
+		 		<div class="well" width="100%">
+        			<a class="edit"href="#">
 
-     			<br><br>
-
-		   		<div class="well" width="50%">
-		   		<a class="edit"href="#">
-
-              	<span class="glyphicon glyphicon-edit"></span>
+              	<span class="glyphicon glyphicon-edit" style="font-size:24px;"></span>
         
         		</a>
 
-        		<h3><b>Student Story</b></h3>
-        		<br>
-        		<p>Hey Guys! Please sponsor me for my new adventure with Jaaga. Currently, I have learnt HTML, CSS and Javascript on my own through Codecademy.
-        		<br>
-        		
+        			<h2>My Story</h2>
+
+        			<br>
+
+        			<p style="text-align:justify">Hey Guys! Please sponsor me for my new adventure with Jaaga. </b>Currently, I have learnt HTML, CSS and Javascript on my own through Codecademy.
+ 					Through this program I am hoping to get a better understanding of programming languages and make great websites for a living!<br><br>
  
-    				
-				</p>
-    				</div>
-		
+        			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna.
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. 
+					Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+					<br> <br>
+					Aenean lacinia bibendum nulla sed consectetur.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna.
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. 
+					Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+					Aenean lacinia bibendum nulla sed consectetur.
+					</p>
+    			</div>
 			</div>
 
-			<div class="col-md-4">
-    			
-				
+		
+			<div class="col-xs-5"> 
 
-    			<br><br>
-				
+				<div class="well" style="background-color:#33cc66; height:400px" id="bg" >
+					<div class="sponsor_data">
+						<h2 class="sponsor_data"> <b>34</b> donors backed </h2><br>
+						<h2 class="sponsor_data"> <b>Rs.40,000</b> pledged<br> out of<b> Rs.1,20,000</b></h2>
+						<h2 class="sponsor_data"> <b>4</b> days to go!</h2>
+					
+						<br>
+					
+						<!--<div class="input-group" style="border: 3px solid #33cc66 ; border-radius: 7px;position:absolute;">
+							<input type="text" class="form-control" placeholder="enter amount" style="height: 50px; ">
+                    	</div> -->
+						
+						<!--<button type="button" class="btn btn-lg btn-success" style="width:150px; height: 50px; float: right;border: 3px solid #33cc66 ;">Donate
+						</button>-->
+						
+					</div>
 
- 				<div class="well" width="50%" id="prework">
+					<br><br><br>			
+				</div>
 
-					<a class="edit" href="#" >
+				<br>
 
-            			<span class="glyphicon glyphicon-edit"></span>
-          			</a>
-  					<h3>PreWork Status </h3>
-    				<p>CodeAcademy</p>
-      				<div class="progress">
-        				<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-        					<span class="sr-only">40% Complete (success)</span>
-        				</div>
-      				</div>
+				<div class="well" style="background-color:#; height:450px; margin-top: -20px; text-align: justify;">
 
-     				<p>CodeSchool</p>
-     				<div class="progress">
-        				<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-    						<span class="sr-only">20% Complete</span>
-    					</div>
+				<a class="edit"href="#">
+
+              	<span class="glyphicon glyphicon-edit" style="font-size:24px;"></span>
+        
+        		</a>
+
+					<h3>So here's why I need your help</h3><br><p><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b> Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. 
+						Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+					</p>
+					<p>Aenean lacinia bibendum nulla sed consectetur.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. </p>
+				</div>
+			</div>
+
+		</div>
+	
+
+    	<div class="row">
+      
+        	<div class="col-sm-7"  text-align="justify">
+
+				<div class="workstatus">
+					<h3>PreWork Status</h3>
+
+					<p>CodeAcademy</p>
+
+					<div class="progress">
+        				<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" 		style="width: 40%">
+							<span class="sr-only">40% Complete (success)</span>
+						</div>
     				</div>
 
-    				<p>TeamTreehouse</p>
-      				<div class="progress">
-        				<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 10%">
-        					<span class="sr-only">40% Complete (success)</span>
-        				</div>
-      				</div>
-
-
-    				<p>CodeJam</p>
-      				<div class="progress">
-        				<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 65%">
-        					<span class="sr-only">40% Complete (success)</span>
-        				</div>
-      				</div>
-
-      				<p>W3School</p>
-      				<div class="progress">
-        				<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 85%">
-        					<span class="sr-only">40% Complete (success)</span>
-        				</div>
-      				</div>
-	  
+	 				<p>Codeschool</p>
+	 				<div class="progress">
+        					<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+							<span class="sr-only">20% Complete</span>
+							</div>
+					</div>	
 				</div>
 			</div>
 
 
-			<div class="col-md-4">
-				
-				<div class="viewprofile">
-   					<form action="studentprofile_v7.html">
-       					<button type="submit" class="btn btn-success" style="font-family: verdana;">&nbsp&nbsp&nbspView Profile&nbsp&nbsp&nbsp</button>
-       				</form>
-       				<form action="application.html">
+			<div class="col-xs-5">
 
-       					<button type="submit" class="btn btn-success" style="font-family: verdana;">Edit Application</button>
-    				
-    				</form>
-    			</div>	
+				<div class="well" width="100%">
+
+				This is where the badges will be placed
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+
+				</div>
+			</div>
+	
 
 
-    			<div class="well" style="background-color:#33cc66; height:200px" id="bg" >
-			  
-						<div class="sponsor_data">
-							<h2> <b>34 </b>Donors Backed </h2>
-							<h2> <b>Rs.40,000</b> Pledged </h2>
-							<h2> <b>4</b> days to go</h2>
-  							
-    					</div>
+    	<div class="row" style="padding: 100px 50px 0 50px" align="center">
+    	<br><br>
 
-  				</div>
+      		<div class="col-lg-4">
 
-  				<div class="well" width="30%">
-  					<a class="edit"href="#">
+          		<img src="./images/Donation.jpg" alt="donate" class="img-circle" align="center" style="Height:100px"></img> 
+          		<br><h3>MAKE A DONATION</h3>
+         		<!-- <p style="text-align: justify; padding:1em 1em;">Our system is simple. We select candidates for a scholarship based on certain criteria preset by our team.We consider economic background, academic performance and extra curricular skills while making our selections, but also look for that x-factor while making our choices.</p>
 
-             			<span class="glyphicon glyphicon-edit"></span>
-           			 </a>
-        			<h3><b>How I will spend your money</b></h3>
-        			<br>
-        			<p>Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem 
+          		<br>-->
+      		</div>
 
-        			<br><br>
- 
-    				
-					</p>
-    			</div>
+      		<div class="col-lg-4">
+        		<img src="./images/Student_benefit.jpg" alt="donate" class="img-circle" style="height:100px"></img> 
+        		<br><h3>STUDENT BENEFITS FROM LOAN</h3>
+        		<!--<p style="text-align: justify; padding:1em 1em;">You as a donor choose the candidate you would like to sponsor. The student utilizes the loan for paying his/her academic fee and to cover basic necessities during the course.</p>
+          		<br><br>-->
+      		</div>
 
-        	</div>
+      		<div class="col-lg-4">
+          		<img src="./images/Pay_it.jpg" alt="donate" class="img-circle" style="height:100px"></img> 
+          		<br><h3>STUDENT PAYS IT FORWARD</h3>
+          		<!--<p style="text-align: justify; padding:1em 1em;">The student who has been sponsored through our system gets inducted into an honour-based pay-it-forward process wherein once the student graduates and begins to earn, he/she is encouraged to sponsor another student.</p>
+      		-->
+      		</div>
     	</div>
-    </div>
-    
-        
+
+  	</div>
+
 	<footer class="footer" style="text-align:center">
   		<nav>
     		<ul>
-    			<a href="home_v7.html">Home </a> |
+    			<a href="home_v3.html">Home </a> |
     			<a href= "#about">About Us</a> |
       			<a href= "#hiw">How It Works</a> |
       			<a href="#faq">FAQ</a> |
       			<a href="#contact">Contact</a>  
+
     		</ul>
   		</nav> 
 	</footer>
