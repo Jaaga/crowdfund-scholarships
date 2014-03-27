2cfe2f8430d14220ebf305c0f098c6d9456c5b21
Sujay Datta <sujaydatta@gmail.com>
2014-03-27 18:18:48 +0530

    updated sign in button font - aboutus

--- public/index.php
+++ public/index.php
@@ -93,7 +93,7 @@ background-repeat:repeat;
 <div class="navbar-collapse collapse">
               <div class="navbar-form navbar-right">
                 <button type="button" class="btn btn-danger">Sign Up</button>
-                <button class="btn btn-success" style="font-family: verdana;" data-toggle="modal" data-target="#myModal">Sign in</button> 
+                <button class="btn btn-success" data-toggle="modal" data-target="#myModal">Sign in</button> 
               
 
               </div>
