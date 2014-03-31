e91fe0b103d627cc3a0aad64ca56c280b0a66abf
abhinay302 <abhinay302@gmail.com>
2014-03-31 09:54:14 +0530

    Update_DB

--- db/LearnEmp.sql
+++ db/LearnEmp.sql
@@ -18,17 +18,19 @@
 --
 -- Table structure for table `donation`
 --
-CREATE Database LearnEmp;
-use LearnEmp;
 
 DROP TABLE IF EXISTS `donation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `donation` (
-  `D_id` int(20) DEFAULT NULL,
+  `U_id` int(20) DEFAULT NULL,
   `S_id` int(20) DEFAULT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `amount` decimal(15,2) DEFAULT NULL
+  `amount` decimal(15,2) DEFAULT NULL,
+  KEY `S_id` (`S_id`),
+  KEY `U_id` (`U_id`),
+  CONSTRAINT `donation_ibfk_1` FOREIGN KEY (`S_id`) REFERENCES `student` (`S_id`),
+  CONSTRAINT `donation_ibfk_2` FOREIGN KEY (`U_id`) REFERENCES `user` (`U_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -40,32 +42,6 @@ LOCK TABLES `donation` WRITE;
 /*!40000 ALTER TABLE `donation` DISABLE KEYS */;
 INSERT INTO `donation` VALUES (1,1,'2014-03-26 09:52:10',40.00),(2,1,'2014-03-26 09:52:30',50.00),(3,1,'2014-03-26 09:52:43',50.00),(1,2,'2014-03-27 13:30:34',0.00),(2,2,'2014-03-27 13:40:45',23.00),(1,2,'2014-03-27 14:05:37',500.00),(2,3,'2014-03-27 14:07:12',600.00);
 /*!40000 ALTER TABLE `donation` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `donor`
---
-
-DROP TABLE IF EXISTS `donor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `donor` (
-  `D_id` int(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL,
-  `email` varchar(320) DEFAULT NULL,
-  PRIMARY KEY (`D_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `donor`
---
-
-LOCK TABLES `donor` WRITE;
-/*!40000 ALTER TABLE `donor` DISABLE KEYS */;
-INSERT INTO `donor` VALUES (1,'abhinay','abhinay','abhinay302@gmail.com'),(2,'freeman','freeman','freeman@gmail.com'),(3,'akshay','akshay','akshay@gmail.com');
-/*!40000 ALTER TABLE `donor` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -83,7 +59,7 @@ CREATE TABLE `scholarship` (
   `S_id` int(20) DEFAULT NULL,
   PRIMARY KEY (`SC_id`),
   KEY `S_id` (`S_id`),
-  CONSTRAINT `scholarship_ibfk_1` FOREIGN KEY (`S_id`) REFERENCES `student` (`S_Id`)
+  CONSTRAINT `scholarship_ibfk_1` FOREIGN KEY (`S_id`) REFERENCES `student` (`S_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -132,6 +108,32 @@ LOCK TABLES `student` WRITE;
 INSERT INTO `student` VALUES (1,'abhinay','kumar','M','abhinay302@gmail.com',2147483647,'Bangalore',560062,'Armenia','Php',120000,'Hi,I am a student seeking scholarship for my higher education. I have learnt HTML, CSS and Javascript on my own through Codecademy & Teamtreehouse.','123456','images/bug.jpg','2014-03-27 13:17:04'),(2,'Vaibhav','Mule','M','vaibhav@gmail.com',2147483647,'Bangalore',560062,'Armenia','Php',120000,'Hey Guys! Please sponsor me for my new adventure with Jaaga. Currently, I have learnt HTML, CSS and Javascript on my own through Codecademy.','123123','images/Freeman.jpg','2014-03-27 13:19:24'),(3,'Micheal','Hartl','M','freeman@gmail.com',2147483647,'Bangalore',560062,'Armenia','html/css',120000,'Hey Guys! Please sponsor me for my new adventure with Jaaga. Currently, I have learnt HTML, CSS and Javascript on my own through Codecademy and codeschool.','123123','images/Micheal.jpg','2014-03-27 13:20:28');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `U_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`U_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -142,4 +144,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-03-27 19:58:35
+-- Dump completed on 2014-03-31  9:52:04
