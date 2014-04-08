ab574c7250f3bde190708ff3b894f7a35e733a99
Rajeef <rajeefmk@gmail.com>
2014-04-08 18:42:52 +0530

    Modal added to Edit application userdashboard

--- public/userdashboard.php
+++ public/userdashboard.php
@@ -21,21 +21,21 @@
   <title>Donor Dashboard</title>
   <link href="./dist/css/bootstrap.css" rel="stylesheet">
   <link href="./dist/css/custom.css" rel="stylesheet" type='text/css'>
- <!-- <link href='http://fonts.googleapis.com/css?family=Cabin+Sketch' rel='stylesheet' type='text/css'>-->
+  <!-- <link href='http://fonts.googleapis.com/css?family=Cabin+Sketch' rel='stylesheet' type='text/css'>-->
   <style type="text/css">
-@font-face{
+    @font-face{
       font-family:'KGSecondChancesSketch'; 
       src:url('fonts/KGSecondChancesSketch.ttf');
 
      /* font-family: 'CabinSketch';
       src: url('fonts/CabinSketch-Regular.otf');*/
-         }
+    }
   
     body{
      
       background-repeat:repeat;
 
-    padding-top: 80px;
+      padding-top: 80px;
 
     }
     
@@ -50,7 +50,7 @@
     display: block;
     height: auto;
     max-width: 100%;
-  }
+    }
   </style>
 </head>
 
@@ -65,20 +65,21 @@
     
           
         		<a class="navbar-brand" href="index.php"><h1 style="font-family:'KGSecondChancesSketch' cursive; margin-top: -9px;">LearnEm</h1></a>
-        	</div>
+        </div>
 
-        	<div class="collapse navbar-collapse">
+        <div class="collapse navbar-collapse">
             	<ul class="nav navbar-nav">
                 <li><a href="listofstudents.php?U_id=<?php echo $U_id; ?>">AllStudents</a></li>
            		</ul>
 
         		<div class="navbar-collapse collapse">
              		<div class="navbar-form navbar-right"> 
-              </div><!--/.navbar-collapse -->
+                </div><!--/.navbar-collapse -->
             </div>
-            	</div><!--/.navbar-collapse -->
-        	</div>      	
-    	</div> 
+        </div><!--/.navbar-collapse -->
+      </div>      	
+  </div> 
+
 <?php 
 $students=getStudents($U_id); 
 $noofStudents=count($students);
@@ -91,7 +92,7 @@ $noofStudents=count($students);
      		<div class="well" >
       
         		<img src="<?php echo $url; ?>" alt="Please Update Your Picture" width="200px" height="200px" style="margin:10px;    margin-right:30px;"/>
-        		<button type="submit" class="btn btn-success" style="font-family: verdana; float:right; margin-right:50px; width:150px; margin-top:20px;"><h4>Edit Profile</h4></button>
+        		<button class="btn btn-success" style="font-family: verdana; float:right; margin-right:50px; width:150px; margin-top:20px;" data-toggle="modal" data-target="#myModal"><h4>Edit Profile</h4></button>
         		<h2><?php echo $row['name'];?> </h2>
         		<br>
         		<p style="font-size:26px" "text-type:bold">Donated to <?php echo $noofStudents ; ?> Students -- Donate to More</p>
@@ -107,7 +108,7 @@ $noofStudents=count($students);
 
     	<br>  
     </div>
-<?php 
+    <?php 
 
     foreach($students as $student):
                
@@ -117,56 +118,53 @@ $noofStudents=count($students);
                 $requiredamount = $student['scholar_AMT'];
                 $percentage = (($sum_amount/$requiredamount)*100);
             
-  ?>
+    ?>
   
   	<div class="row">
-          <a href="<?php echo $profilelink; ?>" style="text-decoration:none;color:#111111;">
-    	<div class="col-md-4" >
+          <a href="<?php echo $profilelink; ?>" style="text-decoration:none;color:#111111;"/>
+    	    <div class="col-md-4" >
         	<div class="well" >
-        	           <!-- style="width: 340px;"-->
-          		<img src=<?php echo $student['image_path']; ?> alt="donate"align="center" style="height:200px; width:320px" ></img> 
-          		<br>
+        	   <!-- style="width: 340px;"-->
+          	 <img src=<?php echo $student['image_path']; ?> alt="donate"align="center" style="height:200px; width:320px" ></img> 
+          	 <br>
 
-          		<h3><?php echo $student['sname'];?></h3>
-          		<p ><?php echo $student['para'];?> </p></a>
+          	 <h3><?php echo $student['sname'];?></h3>
+          	 <p ><?php echo $student['para'];?> </p></a>
 
-          		<div class="progress progress-striped">
+          	 <div class="progress progress-striped">
                 
-          		<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-    valuemax="100" style="width: <?php echo $percentage; ?>%">
-              		<b style="color:#111111;"><?php echo $percentage; ?> Complete</b>
+          		  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-    valuemax="100" style="width: <?php echo $percentage; ?>%">
+              	 <b style="color:#111111;"><?php echo $percentage; ?> Complete</b>
            			</div>
-          		</div>
+          	 </div>
 
-          		<p style="text-align:left"><b>4</b> days to go <b><?php echo "Rs. ".$sum_amount;?></b> 
-              pledged out of <?php echo $requiredamount; ?>
-            		</p>
-      <div class="row">
-        <form method="POST" action="donate.php">
-          <input type="hidden" name="U_id" value="<?php echo $U_id ; ?>" >
-          <input type="hidden" name="S_id" value="<?php echo $student['S_id']; ?>" >
-         <div class="col-md-8">
+          	 <p style="text-align:left"><b>4</b> days to go <b><?php echo "Rs. ".$sum_amount;?></b> 
+                pledged out of <?php echo $requiredamount; ?>
+              </p>
+      
+              <div class="row">
+              <form method="POST" action="donate.php">
+                <input type="hidden" name="U_id" value="<?php echo $U_id ; ?>" >
+                <input type="hidden" name="S_id" value="<?php echo $student['S_id']; ?>" >
+                <div class="col-md-8">
 
-            <input type="text" name="amount" placeholder="$ " class="form-control input-md" > 
+                <input type="text" name="amount" placeholder="$ " class="form-control input-md" > 
+                </div>
 
-          </div>
-
-          <div class="col-md-4">
+                <div class="col-md-4">
             
-              <!--<div class="col-lg-6">-->
-                <input type="submit" value="Donate" class="btn btn-primary">
-
-                             
+                <!--<div class="col-lg-6">-->
+                <input type="submit" value="Donate" class="btn btn-primary">                 
+                </div>
+              </form>
+              </div>
           </div>
-
-      </form>
-      </div>
-      </div>
-     	</div>
-    
+     	    </div>
+    </div>
 
 
 
-  <?php endforeach; ?>
+    <?php endforeach; ?>
   </div>
   <hr>
 
@@ -185,8 +183,65 @@ $noofStudents=count($students);
     </footer>
   </div>
 
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Edit Your Profile</h4>
+      </div>
+      <div class="modal-body">
+        
+        <div class="form-group">
+                  <label class="col-md-4 control-label" for="textinput">Your Name</label>  
+                    <div class="col-md-4">
+                      <input id="name" name="name" type="text" placeholder="Full Name" class="form-control input-md"> 
+                    </div>
+                </div>
+
+                <br>
+                <br>
+
+                <div class="form-group">
+                  <label class="col-md-4 control-label" for="textinput">Old Password</label>  
+                    <div class="col-md-4">
+                      <input id="password" name="password" type="password" placeholder="Old Password" class="form-control input-md"> 
+                    </div>
+                </div>
+
+                <br><br>
+
+                <div class="form-group">
+                  <label class="col-md-4 control-label" for="textinput">New Password</label>  
+                    <div class="col-md-4">
+                      <input id="password" name="password" type="password" placeholder="New Password" class="form-control input-md"> 
+                    </div>
+                </div>
+
+                <br><br>
+
+                <div class="form-group">
+                  <label class="col-md-4 control-label" for="textinput">Confirm Password</label>  
+                    <div class="col-md-4">
+                      <input id="password" name="password" type="password" placeholder="Confirm Password" class="form-control input-md"> 
+                    </div>
+                </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
-</div>
+
+
+
+
+<script type="text/javascript" src="./dist/js/jquery-2.1.0.min.js"></script>
+  <script type="text/javascript" src="./dist/js/bootstrap.min.js"></script>
+
 </body>
+
 
 </html> 
