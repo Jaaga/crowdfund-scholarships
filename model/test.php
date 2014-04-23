0a372fa6222257062f8e72a60422405c3d24b7f3
abhinay302 <abhinay302@gmail.com>
2014-04-23 23:52:26 +0530

    Share_button_LinkedIN

--- model/test.php
+++ model/test.php
@@ -1,8 +1,17 @@
 <?php ?>
 <html>
+<head>
+	<script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript">stLight.options({publisher: "efbad282-59ff-4ef1-90cc-48691ddef625", doNotHash: false, doNotCopy: false, hashAddressBar: true});</script>
+</head>
 <body>
-<a href="https://www.facebook.com/sharer.php?app_id=113869198637480&sdk=joey&u=http%3A%2F%2Flearnem.jaaga.us%2Fpublic%2Fstudentprofile.php%3FS_id%3D%253C%253Fphp%2Becho%2B%2524S_id%2B%253F%253E&display=popup">
-	<img src="./images/Twitter_logo.png" width="25px"/></a>
-</a>
+<span class='st_sharethis_large' displayText='ShareThis'></span>
+<span class='st_facebook_large' displayText='Facebook'></span>
+<span class='st_twitter_large' displayText='Tweet'></span>
+<span class='st_linkedin_large' displayText='LinkedIn'></span>
+<span class='st_pinterest_large' displayText='Pinterest'></span>
+<span class='st_email_large' displayText='Email'></span>
+
 </body>
 </html>
