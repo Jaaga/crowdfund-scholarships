2f40b93c68560f2d2648d2d1982798f9c212d00f
Rajeef <rajeefmk@gmail.com>
2014-04-24 11:39:17 +0530

    form validation application.php

--- public/listofstudents.php
+++ public/listofstudents.php
@@ -223,7 +223,7 @@ CoinWidgetCom.go({
   <footer class="footer" style="text-align:center">
     <nav>
       <ul style="padding-left: 0px;">
-        <a href="index.php">Home </a> |
+        <a href="index.php">Home </a> |http://localhost/public/listofstudents.php
         <a href= "aboutus.php">About Us</a> |
         <a href= "#hiw">How It Works</a> |
         <a href="#faq">FAQ</a> |
@@ -235,7 +235,9 @@ CoinWidgetCom.go({
 </div>
 
 
-    <script type="text/javascript" src="./dist/js/validator.js"></script> 
+    <script type="text/javascript" src="./dist/js/validator.js"></script>
+    <script type="text/javascript" src="./dist/js/jquery-2.1.0.min.js"></script>
+    <script type="text/javascript" src="./dist/js/bootstrap.min.js"></script> 
 </body>
 
 </html> 
